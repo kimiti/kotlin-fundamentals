231cc97d6f073754f8b95b35e2550c49b2f68517
kimiti <kimitikevin@gmail.com>
2020-01-03 08:00:00 +0300

    kotlin variables

---
+++ src/02_variables.kt
@@ -0,0 +1,25 @@
+
+/*
+*   variables & Data types
+*/ 
+val myText: String = "Hello"        // String
+val myNum: Int = 5                  // Int
+val myDoubleNum: Double = 5.99      // Double
+val myLetter: Char = 'D'            // Char
+val myBoolean: Boolean = true       // Boolean
+    
+
+
+/*
+*   update variables
+*/ 
+var cartTotal = 0
+cartTotal = 20
+println("Total: $cartTotal")
+
+
+/*
+*   String template
+*/ 
+println("You have $count unread messages.")
+println("You have ${unreadCount + readCount} total messages in your inbox.")
